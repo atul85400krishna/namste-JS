a83205cda2575c04c4bb46536bda40cb0ce6609b
atul85400krishna <atul85400krishna@gmail.com>
2023-02-05 12:06:24 +0530

    class2

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-var n=2;
-function square(num){
-    var ans=num*num;
-    return ans
+// var n=2;
+// function square(num){
+//     var ans=num*num;
+//     return ans
+// }
+// var square2=square(n)
+// var square3=square(4)
+
+
+
+var x=7
+function getname(){
+    console.log("namste javascript")
 }
 
-var square2=square(n)
-var square3=square(4)
+getname();
+console.log(x)
